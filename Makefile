fefd05ed61b4ddcf73e0136a0288c10157cb56b2
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2016-11-25 13:10:25 +0300

    relational split for expressions

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 .PHONY: all test clean
 
-#OCB_FLAGS = -use-ocamlfind -cflag "-g" -cflag "-rectypes" -lflag "-g" -pkgs GT.syntax.all -pkgs ocanren.syntax -pkgs oUnit -pkgs ocanren -I src -I test -syntax 'camlp5' -ppflag "pr_dump.cmo"
-OCB_FLAGS = -use-ocamlfind -syntax camlp5o -cflag "-g" -cflag "-rectypes" -lflag "-g" -pkgs GT.syntax.all -pkgs ocanren.syntax -I src -I test 
+OCB_FLAGS = -use-ocamlfind -syntax camlp5o -cflag "-g" -cflag "-rectypes" -lflag "-g" -pkgs GT.syntax.all -pkgs ocanren.syntax -pkgs oUnit -pkgs ocanren -I src -I test 
 OCB = ocamlbuild $(OCB_FLAGS)
 
 all: 
--- src/logic.ml
+++ src/logic.ml
@@ -1,13 +1,7 @@
 open GT
 open MiniKanren
 
-@type ('int, 'string, 't, 'c) ac = 
-      | Hole
-      | BinopL of 'string * 'c * 't
-      | BinopR of 'string * 't * 'c
-      with gmap, show
-
-module ExprContext =
+module ExprTerm = 
   struct
     @type ('int, 'string, 't) at =
       | Const of 'int
@@ -25,32 +19,57 @@ module ExprContext =
     let rec prj_term : lt -> t = fun lt ->
       gmap(at) (prj) (prj) (prj_term) (prj lt)
 
-    let rec show_term t = show(at) (show(int)) (show(string)) (show_term) t  
+    let rec show_term t = show(at) (show(int)) (show(string)) (show_term) t
 
-    
+    let is_value_term = function
+      | Const _ -> true
+      | _       -> false
+
+    let is_value_termo t = fresh (n) (t === !!(Const n)) 
+  end
+
+module ExprContext =
+  struct
+    type t  = ExprTerm.t
+    type lt = ExprTerm.lt
+
+    @type ('int, 'string, 't, 'c) ac = 
+      | Hole
+      | BinopL of 'string * 'c * 't
+      | BinopR of 'string * 't * 'c
+      with gmap, show
 
     type c  = (int, string, t, c) ac
     type lc = (int logic, string logic, lt, lc) ac logic
 
     let rec inj_context : c -> lc = fun c ->
-      inj (gmap(ac) (inj) (inj) (inj_term) (inj_context) c)
+      inj (gmap(ac) (inj) (inj) (ExprTerm.inj_term) (inj_context) c)
 
     let rec prj_context : lc -> c = fun lc ->
-      gmap(ac) (prj) (prj) (prj_term) (prj_context) (prj lc)
+      gmap(ac) (prj) (prj) (ExprTerm.prj_term) (prj_context) (prj lc)
 
-    let rec show_context c = show(ac) (show(int)) (show(string)) (show_term) (show_context) c 
+    let rec show_context c = show(ac) (show(int)) (show(string)) (ExprTerm.show_term) (show_context) c 
 
-    let rec splito t ct = conde [
-      (fresh (n) ((t === !!(Const n)) &&& (ct === !!(!!Hole, t))));
-      (fresh (x) ((t === !!(Var x))   &&& (ct === !!(!!Hole, t))))
-    ]
+    let rec splito t ct = ExprTerm.(conde [
+      fresh (op l r ct' c' t')
+         (t === !!(Binop (op, l, r)))
+         (ct' === !!(c', t')) 
+         (conde [
+          ((ct === !!(!!Hole, t))                  &&& (is_value_termo l) &&& (is_value_termo r));
+          ((ct === !!(!!(BinopR (op, l, c')), t')) &&& (splito r ct')     &&& (is_value_termo l));
+          ((ct === !!(!!(BinopL (op, c', r)), t')) &&& (splito l ct'));
+        ]);
+      (ct === !!(!!Hole, t));
+    ])
   end 
 
 let _ = 
+  let module ET = ExprTerm in
   let module EC = ExprContext in
-  let e = EC.Var "x" in
-    run q (fun q -> EC.splito (EC.inj_term e) q)
-          (fun a -> (fun (lc, lt) -> print_string @@ show @@ EC.prj_context lc;
-                                     print_string @@ show @@ EC.prj_term lt
-                                     ) @@ Stream.hd a)
+  let e = ET.Binop ("+", ET.Var "x", ET.Const 2) in
+    run q (fun q -> EC.splito (ET.inj_term e) q)
+          (fun a -> (fun (lc, lt) -> print_string  @@ EC.show_context @@ EC.prj_context lc;
+                                     print_string  @@ ", ";
+                                     print_endline @@ ET.show_term @@ ET.prj_term lt
+                                     ) @@ prj @@ Stream.hd a)
   
